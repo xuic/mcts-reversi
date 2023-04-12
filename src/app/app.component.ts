f19d8372132efbe7b10ef1b4176b0dd30e9687c5
xuic <dear991082@gmail.com>
2023-04-13 01:35:48 +0800

    update angular version from 14 to 15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [NgSwitch, NgSwitchDefault, NgSwitchCase],
 })
 export class AppComponent {
   title = 'mcts-reversi';
